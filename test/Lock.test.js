1b257ade715d5277f551c87696063ece7f05a192
wighawag <wighawag@gmail.com>
2023-06-03 06:53:09 +0100

    vitest

--- test/Lock.test.js
+++ test/Lock.test.js
@@ -1,9 +1,12 @@
-const {
+import { describe, it } from 'vitest';
+import {ethers} from 'hardhat';
+
+import {
   time,
   loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+} from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
 
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
