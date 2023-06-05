72b40ebb7e92104f3940d30d0d8d4ac743fb6d7b
wighawag <wighawag@gmail.com>
2023-06-05 21:41:05 +0100

    remove

--- external/rocketh-verifier/src/cli.ts
+++ external/rocketh-verifier/src/cli.ts
@@ -7,4 +7,4 @@ const args = process.argv.slice(2);
 
 const resolvedConfig = readAndResolveConfig({network: args[0]}, {ignoreMissingRPC: true});
 
-run(resolvedConfig, {verifier: {type: 'etherscan', apiKey: process.env['ETHERSCAN_API_KEY']}, logErrorOnFailure: true});
+run(resolvedConfig, {verifier: {type: 'etherscan', apiKey: process.env['ETHERSCAN_API_KEY']}});
